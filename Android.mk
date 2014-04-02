9595d334c6ad6fafda0f4db66a4080dde08b6143
Drgravy <drg113001@gmail.com>
2014-04-02 16:13:40 -0500

    update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,8 +1,5 @@
-ifneq ($(filter msm8960 msm8660 msm7x30,$(TARGET_BOARD_PLATFORM)),)
-ifeq ($(TARGET_QCOM_MEDIA_VARIANT),caf)
-
-include $(call all-subdir-makefiles)
-
+ifeq ($(TARGET_QCOM_MEDIA_VARIANT),caf-v500)
+	ifeq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
+	        include $(call all-subdir-makefiles)
+	endif
 endif
-endif
-
