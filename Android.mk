7a1401efde7cc4b12f040577f35a826fd7cf140d
Drgravy <drg113001@gmail.com>
2014-04-03 19:22:56 -0500

    fix Android.mk

--- Android.mk
+++ Android.mk
@@ -1,5 +1,7 @@
+ifneq ($(filter msm8960 msm8660 msm7x30,$(TARGET_BOARD_PLATFORM)),)
 ifeq ($(TARGET_QCOM_MEDIA_VARIANT),caf-v500)
-	ifeq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
-	        include $(call all-subdir-makefiles)
-	endif
+
+include $(call all-subdir-makefiles)
+
+endif
 endif
